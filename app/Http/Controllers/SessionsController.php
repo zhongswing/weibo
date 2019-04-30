5896e03639708183818051304e99947c9504d1e5
vagrant <vagrant@homestead>
2019-04-30 21:27:49 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,4 +26,10 @@ class SessionsController extends Controller
         return redirect()->back()->withInput();
       };
     }
+
+    public function destory(){
+      Auth::logout();
+      session()->flash('success','您成功推出');
+      return redirect('login');
+    }
 }
