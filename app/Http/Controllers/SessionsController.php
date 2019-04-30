537d2d4a7b87332f2bbaf9d906c2129aae757dfd
vagrant <vagrant@homestead>
2019-04-30 21:32:35 +0000

    登陆时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -18,7 +18,7 @@ class SessionsController extends Controller
         'email' => 'required|email|max:255',
         'password' => 'required'
       ]);
-      if (Auth::attempt($credentials)){
+      if (Auth::attempt($credentials,$request->has('remember'))){
         session()->flash('success','welcome back!');
         return redirect()->route('users.show',[Auth::user()]);
       }else{
