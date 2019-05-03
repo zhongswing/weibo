e09bb9d1ceede69627d27a195070c4b963f2dd8b
vagrant <vagrant@homestead>
2019-05-03 21:54:24 +0000

    修改文案

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -19,9 +19,9 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
+      <h1>Hello！</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+        你现在所看到的是入门教程的示例项目主页。
       </p>
       <p>
         一切，将从这里开始。
