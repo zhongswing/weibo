33cf646d6fb85bf4c1e4be7a6fae42ae671d6d4a
zhongswing <278468924@qq.com>
2019-05-10 19:04:18 +0000

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -15,13 +15,16 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
+        <section class="stats mt-2">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
       </aside>
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello！</h1>
+      <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是入门教程的示例项目主页。
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
       </p>
       <p>
         一切，将从这里开始。
