7188832e280ceeed40e017bd82179fb4a172f228
zhongswing <278468924@qq.com>
2019-05-11 23:28:07 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,8 +62,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-      return $this->statuses()
-                  ->orderBy('created_at','desc');
+      $user_ids = $this->followings->pluck('id')->toArray();
+      array_push($user_ids,$this->id);
+      return Status::whereIn('user_id',$user_ids)
+                            ->with('user')
+                            ->orderBy('created_at','desc');
     }
 
     public function followers()
@@ -96,5 +99,6 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
 
 }
